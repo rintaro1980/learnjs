903685359ae847a3497f56a4e7f14d2f98624254
Rintaro <>
2022-08-09 10:51:04 +0900

    chapter2

--- public/app.js
+++ public/app.js
@@ -1,23 +1,48 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function() {
-    return $('<div class="problem-view">').text('Coming soon!');
-}
+// learnjs.problemView = function() {
+//     return $('<div class="problem-view">').text('Coming soon!');
+// }
 
 // routesは連想配列として、引数であるhashを用いて
 // routes[hash]で一致する#problem-1に合致する
 // valueをviewFnにセットして、中身が空なら何もしない。
 // からじゃなければ、view-containerの子要素である'landing-view'を空にして、viewFn
 // の中身であるlearnjs.problemViewを実行してHtmlを書き換えている。
+// learnjs.showView = function(hash) {
+//     console.log(hash)
+//     var routes = {
+//         '#problem-1':learnjs.problemView
+//     };
+
+//     var viewFn = routes[hash];
+//     if (viewFn) {
+//         $('.view-container').empty().append(viewFn());
+//     }
+// };
+
+learnjs.problemView = function(problemNumber) {
+    var title = 'Problem #' + problemNumber + ' Coming soon!'; 
+    return $('<div class="problem-view">').text(title);
+}
+
+
 learnjs.showView = function(hash) {
-    console.log(hash)
     var routes = {
-        '#problem-1':learnjs.problemView
+        '#problem':learnjs.problemView
     };
 
-    var viewFn = routes[hash];
+    var hashParts = hash.split('-');
+    var viewFn = routes[hashParts[0]]
     if (viewFn) {
-        $('.view-container').empty().append(viewFn());
+        $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 };
+
+learnjs.appOnReady = function() {
+    window.onhashchange = function() {
+        learnjs.showView(window.location.hash);
+    };
+    learnjs.showView(window.location.hash);
+}
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -12,4 +12,32 @@ describe('LearnJS', function() {
         learnjs.showView('#problem-1');
         expect($('.view-container .problem-view').length).toEqual(1);
     });
+
+    it('involes the router when loaded', function(){
+        spyOn(learnjs, 'showView'); //spyOnで（オブジェクト、’メソッド’）のメソッドを監視
+        learnjs.appOnReady();
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
+    it('subscribe to the hash change event', function(){
+        learnjs.appOnReady();
+        spyOn(learnjs, 'showView');
+        $(window).trigger('hashchange');
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
+
+    describe('problem view', function() {
+        it('has a title that includes the problem number', function(){
+            var view = learnjs.problemView('1');
+            expect(view.text()).toEqual('Problem #1 Coming soon!');
+        });
+
+        it('passes the hash view parameter to the view function', function(){
+            spyOn(learnjs,'problemView');
+            learnjs.showView('#problem-42');
+            expect(learnjs.problemView).toHaveBeenCalledWith('42');
+        });
+    
+    });
 });
